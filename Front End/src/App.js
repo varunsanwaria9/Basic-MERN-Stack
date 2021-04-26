e3e55297f776bf89d708c481ea244d691cb28d04
Varun Sanwaria <varunsanwaria9@gmail.com>
2021-04-26 20:09:44 +0530

    Updated

--- Front End/src/App.js
+++ Front End/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-// import Login from './components/Login';
-// import Registration from './components/Registration';
+import Login from './components/Login';
+import Registration from './components/Registration';
 import Admin from './components/Admin';
-// import UserHome from './components/UserHome';
+import UserHome from './components/UserHome';
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
+      <Router>
         <Switch>
-          <Route exact path="/" component={Login} />
+          <Route path="/login" component={Login} />
           <Route path="/reg" component={Registration} />
           <Route path="/userafterlogin" component={UserHome} />
-          <Route path="/adminafterlogin" component={AdminHome} />
+          <Route path="/admin" component={Admin} />
           {/* <Route component={NoMatch} /> */}
-        {/* </Switch> */}
-      {/* </Router>  */}
-      <Admin />
+        </Switch>
+      </Router> 
+    
     </div>
   );
 }
--- Front End/src/components/Admin.js
+++ Front End/src/components/Admin.js
@@ -1,26 +1,25 @@
-import React from 'react'
+import React, { Component }from 'react'
+import { Route,Link } from 'react-router-dom'
 import AdminExam from './AdminExam'
 import AdminUser from './AdminUser'
 import "./CSS/Admin.css"
 
-function Admin() {
-    let whichComp = false
-    const switchComp = () => whichComp = !whichComp
-    return (
+class Admin extends Component{
+    render() {
+        return (
         <div className="admin-main">
             <nav className="admin-navbar">
                 <p>Exam Register</p>
                 <div className="admin-right">
-                    {whichComp && <button onClick={switchComp}>Users</button>}
-                    {!whichComp && <button onClick={switchComp}>Exam</button>}
-                    {whichComp && <button>Add</button>}
+                    <Link to="/admin/"><button>Exam</button></Link>
+                    <Link to="/admin/users"><button>Users</button></Link>
                     <button>Logout</button>
                 </div>
             </nav>   
-            {!whichComp && <AdminUser />}
-            {whichComp && <AdminExam />}
+            <Route exact path="/admin/" component={AdminExam} />
+            <Route path="/admin/users" component={AdminUser} />
         </div>
-    )
+    )}
 }
 
 export default Admin
